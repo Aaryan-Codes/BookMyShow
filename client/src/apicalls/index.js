a4e6e30aba17f80282d5bd4bf50a9f5d185a4aec
Aaryan-Codes <hiaaryansingh@gmail.com>
2024-05-29 16:28:42 +0530

    updated axios baseURL

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  baseURL:'https://bookmyshow-b9r1.onrender.com',
   headers: {
     "Content-Type": "application/json",
     "authorization": `Bearer ${localStorage.getItem("token")}`,
